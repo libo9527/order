0022d68259e567290af2555c89147468b8cb2458
libo <18629374709@163.com>
2018-09-18 23:08:03 +0800

    将库存存放到redis中

--- service/src/main/java/com/imooc/order/message/ProductInfoReceiver.java
+++ service/src/main/java/com/imooc/order/message/ProductInfoReceiver.java
@@ -5,6 +5,8 @@ import com.imooc.product.common.ProductInfoOutput;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,10 +18,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class ProductInfoReceiver {
 
+    private static final String PRODUCT_STOCK_TEMPLATE = "product_stock_%s";
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @RabbitListener(queuesToDeclare = @Queue("productInfo"))
     public void process(String message) {
-        //message => ProductInfoOutput
+        // message => ProductInfoOutput
         ProductInfoOutput productInfoOutput = (ProductInfoOutput) JsonUtil.fromJson(message, ProductInfoOutput.class);
         log.info("从队列【{}】接收到消息：{}", "productInfo", productInfoOutput);
+
+        // 存储到缓存redis中
+        stringRedisTemplate.opsForValue().set(
+                String.format(PRODUCT_STOCK_TEMPLATE, productInfoOutput.getProductId()),
+                String.valueOf(productInfoOutput.getProductStock()));
     }
 }
